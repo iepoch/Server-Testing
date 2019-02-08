4865da306c5fe85e4c53428299ffbdb3d1571873
iepoch <staclem@live.com>
2019-02-07 20:10:32 -0700

    Created Migration file

---
+++ data/migrations/20190207200740_students.js
@@ -0,0 +1,14 @@
+
+exports.up = function (knex, Promise) {
+    return knex.schema.createTable('students', students => {
+        students.increments();
+        students.string('first_name', 128).notNullable();
+        students.string('last_name', 128).notNullable();
+    })
+
+};
+
+exports.down = function (knex, Promise) {
+    return knex.schema.dropTableifExists('students');
+
+};
--- knexfile.js
+++ knexfile.js
@@ -5,9 +5,29 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
+      filename: './data/riverdale.sqlite3'
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
     }
-  }
+  },
+  testing: {
+    client: 'sqlite3',
+    connection: {
+      filename: './data/test.db3'
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './data/test/migrations',
+    },
+    seeds: {
+      directory: './data/test/seeds',
+    },
+  },
 
 
 };
